2dcbfdc279a6a2ee8e9b4cae4d6961f6a3ffc928
Panna <Panna@192.168.2.106>
2012-12-05 02:16:50 +0100

    laptop11

--- test/src/main.java
+++ test/src/main.java
@@ -6,7 +6,7 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test123456");
+		System.out.println("laptopneu");
 	}
 
 }
